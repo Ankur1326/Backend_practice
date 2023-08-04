abe392fe0f5d53afbd8b33fec34c3fc0307f72a2
Ankur1326 <swamiankur46@gmail.com>
2023-08-04 10:12:22 +0530

    update auth_with_express/backend/controller/authController.js

--- auth_with_express/backend/controller/authController.js
+++ auth_with_express/backend/controller/authController.js
@@ -1,3 +1,5 @@
+const userModel = require("../model/userSchema");
+
 const signup = async (req, res, next) => {
     const { name, email, password, confirmPassword } = req.body;
     console.log(name, email, password, confirmPassword);
@@ -16,7 +18,7 @@ const signup = async (req, res, next) => {
             message: 'Please provide a valid email id'
         })
     }
-    
+
     try {
         const userInfo = userModel(req.body);
         const result = await userInfo.save();
@@ -34,6 +36,45 @@ const signup = async (req, res, next) => {
             })
         }
     }
+}
+
+const signin = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({
+            success: false,
+            message: "Every fild is mandatory"
+        })
+    }
+
+    const user = await userModel
+        .findOne({
+            email
+        })
+        .select('+password');
+
+        if (!user || user.password !== password) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid credent"
+            })
+        }
+
+        const token = user.jwtToken();
+        user.password = undefined;
+
+        const cookieOption = {
+            maxAge: 24 * 60 * 60 * 1000,
+            httpOnly: true
+        };
+
+        res.cookie("token", token, cookieOption);
+        res.status(200).json({
+            success: true,
+            data: user
+        })
+
 }
 
 module.exports = {
